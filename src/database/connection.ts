fb291e018a8330a7cb54b6c7d05f06bb9a7cc424
Lucas Lima <38673203+lucaslimaax@users.noreply.github.com>
2020-10-15 21:57:32 -0300

    Add express and create routes schema
    -Add npm express
    -Add Scripts Dev: Typescript
    -Create routes and server file api

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,17 +1,18 @@
-const Mongoose = require('mongoose')
-Mongoose.connect('mongodb://lucas:mypassowe@localhost:27017/login_db', 
+import mongoose from 'mongoose';
+
+ mongoose.connect('mongodb://lucas:mypassowe@localhost:27017/login_db', 
     {useNewUrlParser: true}, function (error){
         if(!error) return;
         console.log('Falaha na conexão!', error)
     })
 
-const connection = Mongoose.connection
+const connection = mongoose.connection
 
 connection.once('open', () => console.log('db Iniciou') )
-    // setTimeout(()=>{
-    //     const state = connection.readyState
-    //     console.log('State',state)
-    // }, 1000)
+    setTimeout(()=>{
+        const state = connection.readyState
+        console.log('State',state)
+    }, 1000)
 
     /*
     0:disconectato
@@ -19,4 +20,3 @@ connection.once('open', () => console.log('db Iniciou') )
     2:conectando
     3:disconectado
     */
-
--- src/schema/Create_user.ts
+++ src/schema/Create_user.ts
@@ -1,3 +1,6 @@
+const Mongoose = require('mongoose')
+
+
 const loginSchema = new Mongoose.Schema({
     name: {
         type: String,
@@ -9,8 +12,44 @@ const loginSchema = new Mongoose.Schema({
         required: true
     },
 
+    password:{
+        type: String,
+        required: true
+    },
+
+    phone: {
+        type: Number,
+        required: false
+    },
+
     insertedAt: {
-        
+        type: Date,
+        default: new Date()    
+    },
+
+    UpdatedAt: {
+        type: Date,
+        default: new Date()    
+    },
+
+    LastLogin: {
+        type: Date,
+        default: new Date()
     }
 
 })
+
+const model = Mongoose.model('Login', loginSchema)
+
+async function main(){
+    const resultCadastrar = await model.create({
+        name: 'Lucas',
+        email: 'lucashendall@gmail.com',
+        password: 'teste@2020',
+
+    })
+
+    console.log('result cadastrar: ', resultCadastrar)
+}
+
+main()
